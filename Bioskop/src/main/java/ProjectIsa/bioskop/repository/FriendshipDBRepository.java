db89e3eeda8cd30a01104504eaceb4ef632fea4b
srbulovicdusan <srbulovicdusan@gmail.com>
2018-06-27 00:11:00 +0200

    u query promenjem naziv tabele, zbog hosta

--- Bioskop/src/main/java/ProjectIsa/bioskop/repository/FriendshipDBRepository.java
+++ Bioskop/src/main/java/ProjectIsa/bioskop/repository/FriendshipDBRepository.java
@@ -22,17 +22,17 @@ public interface FriendshipDBRepository extends JpaRepository<Friendship, Friend
 	Friendship findByPrimKey(FriendshipPrimKey primaryKey);
 	
 	
-	@Query(value = "SELECT * FROM isa.friendship WHERE (userID1 = ?1 OR userID2 = ?1) AND status = 0", nativeQuery = true)
+	@Query(value = "SELECT * FROM friendship WHERE (userID1 = ?1 OR userID2 = ?1) AND status = 0", nativeQuery = true)
 	List<Friendship> nadjiPoKljucu(long userID);
 	
 	List<Friendship> findAllByPrimKey_UserID1OrPrimKey_UserID2(long userID, long userID2);
 	
 	@Query(value = "SELECT temp.* FROM "
-						+ "(SELECT u.* FROM isa.user u "
-						+ "WHERE u.id = (SELECT f.userid2 FROM isa.friendship f "
+						+ "(SELECT u.* FROM user u "
+						+ "WHERE u.id = (SELECT f.userid2 FROM friendship f "
 										+ "WHERE f.userid1 = ?1"
 									+ ") "
-						+ "OR u.id = (SELECT f.userid1 FROM isa.friendship f "
+						+ "OR u.id = (SELECT f.userid1 FROM friendship f "
 										+ "WHERE f.userid2 = ?1"
 									+ ")"
 					+ ") temp",
